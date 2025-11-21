c8fa5f73a6faf4cab5b7712ca00390df73d143e1
Quentin Wood <quentin.wood14159@gmail.com>
2025-11-20 20:47:45 -0700

    Job 1 Okay
    Man I suck at coding

--- Assets/Scripts/Job 2/useMachine.cs
+++ Assets/Scripts/Job 2/useMachine.cs
@@ -1,12 +1,34 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using KinematicCharacterController.Examples;
+
+
 
 public class useMachine : MonoBehaviour
 {
-    public GameObject item;
+    public ExamplePlayer examplePlayer;
+    public static bool machineOpen = false;
+    public PlayerInput playerInput;
+
+
     public Camera playerCamera;
     [SerializeField] public float interactDistance = 3f;
     [SerializeField] public LayerMask interactableMask;
+    [SerializeField] GameObject coffeeMachine;
+
+
+    
     public float useRange = 50f;
+    private Quaternion spawnRot= quaternion.Euler(0f, 0f, 0f);
+    private Vector3 spawnPos = new Vector3(-15, 1, -1.5f);
+
+    public GameObject Coffee1;
+    public GameObject Coffee2;
+    public GameObject Coffee3;
+    public GameObject Coffee4;
+    public GameObject Coffee5;
+    public GameObject Coffee6;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,27 +38,81 @@ public class useMachine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (machineOpen)
+        {
+            return;
+        }
+
         Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        
         if (Physics.Raycast(ray, out RaycastHit hit, interactDistance, interactableMask))
         {
             if (Input.GetKeyDown(KeyCode.U))
             {
-                Transform machine = hit.collider.transform;
+                openMachine();
 
-        // Spawn position = in front of machine + a little upward
-                Vector3 spawnPos = machine.position 
-                           + machine.forward * 0.5f   // forward
-                           + Vector3.up * 0.3f;       // slight upward height
 
-        // Spawn rotation matches the machine's rotation
-                Quaternion spawnRot = machine.rotation;
-
-        // Instantiate the item
-                Instantiate(item, spawnPos, spawnRot);
             }
         
         }
         
+    }
+        public void spawnCoffee1()
+    {
+        Debug.Log("should spawn");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Instantiate(Coffee1, spawnPos, spawnRot);
+    }
+            public void spawnCoffee2()
+    {
+        Instantiate(Coffee2, spawnPos, spawnRot);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+            public void spawnCoffee3()
+    {
+        Instantiate(Coffee3, spawnPos, spawnRot);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+            public void spawnCoffee4()
+    {
+        Instantiate(Coffee4, spawnPos, spawnRot);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+            public void spawnCoffee5()
+    {
+        Instantiate(Coffee5, spawnPos, spawnRot);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+            public void spawnCoffee6()
+    {
+        Instantiate(Coffee6, spawnPos, spawnRot);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void openMachine()
+    {
+        examplePlayer.enabled = false;
+        machineOpen = true;
+        coffeeMachine.SetActive(true);
+        playerInput.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+    }
+    public void closeMachine()
+    {
+        examplePlayer.enabled = true;
+        machineOpen = false;
+        coffeeMachine.SetActive(false);
+        playerInput.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
     }
     
     
